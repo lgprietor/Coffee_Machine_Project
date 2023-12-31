ff5520aa1211542de2cb3ac6911a365ac86a32ca
Luis Gabriel <luisgabrielprieto@gmail.com>
2023-12-31 12:23:05 -0500

    Coffee Machine Project Adjusted

--- main.py
+++ main.py
@@ -10,6 +10,27 @@ print(logo)
 
 money = []
 
+# TODO 15.1: It's necessary to allow dictionary to be a global variable in order to gain access everywhere:
+
+values_paid_dictionary = {
+            "quarter": [],
+            "dime": [],
+            "nickel": [],
+            "penny": [],
+            "Money": [],
+            "Change": [],
+            "Total_money": [],
+            "Refunds": [],
+}
+
+# TODO: 7 - It's necessary to create a dictionary to display on screen for user the coins options (Global)
+
+coins = {
+    "quarter": 0.25,
+    "dime": 0.10,
+    "nickel": 0.05,
+    "penny": 0.01,
+}
 
 # TODO: 3 - Creating a report function that print on screen the balance of ingredients and money. This function was
 #  written to allow user call the function in the ask_user_selection function
@@ -18,16 +39,30 @@ def report():
     print("The machine right now has the following resources: \n")
     for i in resources:
         if i == "water" or i == "milk":
-            print(f"{i.title()} = {resources[i]} mL")
+            print(f"{i.title()}: {resources[i]} mL")
         else:
-            print(f"{i.title()} = {resources[i]} g")
-    print(f"Money: ${sum(money)}")
+            print(f"{i.title()}: {resources[i]} g")
+
+    for i in values_paid_dictionary:
+        if i == "quarter" or i == "dime" or i == "nickel" or i == "penny":
+            #print(f"{i} coins quantity {sum(values_paid_dictionary[i])}")
+            money_calculated = (coins[i])*sum(values_paid_dictionary[i])
+            values_paid_dictionary["Total_money"].append(money_calculated)
+
+    #Print total
+
+    Final_total = sum(values_paid_dictionary["Total_money"]) - sum(values_paid_dictionary["Change"]) \
+                  - sum(values_paid_dictionary["Refunds"])
+    # Final_change = sum(values_paid_dictionary["Change"])
+
+    #print(f"The change returned to user is: {Final_change}")
+    print(f"Money: $USD {Final_total}")
+    # print(values_paid_dictionary)
 
 
 # TODO: 2 - Create a function that validates an input for coffee machine type. According to the
 #  analysis, this validation must be done using a combination of while + if + else because is a string type
 #  The user here has the option of select a type of coffee or he can select off function or report function:
-
 
 def ask_usr_selection():
     usr_selection_validated = False
@@ -43,21 +78,16 @@ def ask_usr_selection():
 
         elif user_selection == "off":
             print("Bye")
-            return
+            return user_selection
 
         elif user_selection == "report":
-            report()
-            return
+            return user_selection
 
         else:
             print("Please write a valid option")
 
 
 user_coffee_selected = ask_usr_selection()
-
-coffee_cost = MENU[user_coffee_selected]["cost"]
-
-print(f"The {user_coffee_selected} costs $USD {coffee_cost}")
 
 
 # print(user_coffee_selected)
@@ -97,20 +127,8 @@ def check_resources(user_coffee_selected):
 
 # TODO: 6 - Create a function to process the coins. This function has more functions inside
 
-resources_status = check_resources(user_coffee_selected)
-
-
-def check_coins_function():
+def check_coins_function(resources_status):
     if resources_status:
-
-        # TODO: 7 - It's necessary to create a dictionary to display on screen for user the coins options
-
-        coins = {
-            "quarter": 0.25,
-            "dime": 0.10,
-            "nickel": 0.05,
-            "penny": 0.01,
-        }
 
         print("This machine receives this type of coins: \n")
 
@@ -189,22 +207,14 @@ def check_coins_function():
 
         calculated_payment = (current_paid_value(coin_selected_translated, coins_paid))
 
-    # TODO: 15 - It's necessary to create a function that saves the quantities of type of coin, and quantity
-    #  calculated to facilitate the calculation in each payment iteration:
-
-        values_paid_dictionary = {
-        "quarter": [],
-        "dime": [],
-        "nickel": [],
-        "penny": [],
-        "Total_paid": []
-        }
+        # TODO: 15 - It's necessary to create a function that saves the quantities of type of coin, and quantity
+        #  calculated to facilitate the calculation in each payment iteration:
 
         def value_saver(coin_selected_translated, coins_paid, calculated_payment):
 
             values_paid_dictionary[coin_selected_translated].append(coins_paid)
 
-            values_paid_dictionary["Total_paid"].append(calculated_payment)
+            values_paid_dictionary["Money"].append(calculated_payment)
 
         value_saver(coin_selected_translated, coins_paid, calculated_payment)
 
@@ -248,10 +258,11 @@ def check_coins_function():
                     change = abs(difference)
 
                     print(f"It was a pleasure to serve you, there you go, you have your "
-                          f"{user_coffee_selected} 🍵🍵🍵🍵 and your {change} change")
+                          f"{user_coffee_selected} 🍵🍵🍵🍵 and your $USD {change} change")
 
+                    values_paid_dictionary["Change"].append(change)
                     payment_checked = True
-                    transaction_complete = True
+                    transaction_checked = True
 
                 elif difference == 0:
 
@@ -259,7 +270,7 @@ def check_coins_function():
                           f"{user_coffee_selected} 🍵🍵🍵🍵")
 
                     payment_checked = True
-                    transaction_complete = True
+                    transaction_checked = True
 
                 while difference > 0:
 
@@ -270,10 +281,9 @@ def check_coins_function():
 
                     if user_continue_payment == "no":
 
-                        print(f"Your funds {calculated_payment} have been refunded")
+                        print(f"Your funds $USD {calculated_payment} have been refunded")
+                        values_paid_dictionary["Refunds"].append(calculated_payment)
                         return
-
-                        transaction_complete = False
 
                     elif user_continue_payment == "yes":
 
@@ -282,7 +292,7 @@ def check_coins_function():
                         coins_paid = coins_used(coin_selected_translated)
                         calculated_payment = (current_paid_value(coin_selected_translated, coins_paid))
 
-                        calculated_payment2 = (calculated_payment + values_paid_dictionary["Total_paid"][-1])
+                        calculated_payment2 = (calculated_payment + values_paid_dictionary["Money"][-1])
 
                         value_saver(coin_selected_translated, coins_paid, calculated_payment2)
 
@@ -292,14 +302,81 @@ def check_coins_function():
 
                         difference = difference2
 
-            return transaction_complete
+            return transaction_checked
 
-        transaction_status = payment_completion(user_coffee_selected, calculated_payment)
+        status_transaction = (payment_completion(user_coffee_selected, calculated_payment))
 
-        print(transaction_status)
+        # TODO: 16 - Create a function that subtracts the quantities of resources used to produce a cup of coffee
+        def quantities_subtraction(status_transaction, user_coffee_selected):
+
+            if status_transaction:
+
+                for i in MENU[user_coffee_selected]["ingredients"]:
+                    resources[i] = resources[i] - MENU[user_coffee_selected]["ingredients"][i]
+
+            else:
+                return
+
+            # print(resources)
+
+        quantities_subtraction(status_transaction, user_coffee_selected)
+
+        # print(values_paid_dictionary)
+
+        return status_transaction
 
     else:
         return
 
+# TODO: 17 - It's necessary to call the function to get a coffee by the first time
 
-check_coins_function()
+if user_coffee_selected == "espresso" or user_coffee_selected == "latte" or user_coffee_selected == "cappuccino":
+    coffee_cost = MENU[user_coffee_selected]["cost"]
+
+    print(f"The {user_coffee_selected} costs $USD {coffee_cost}")
+
+    resources_status = check_resources(user_coffee_selected)
+
+    new_coffee = check_coins_function(resources_status)
+
+elif user_coffee_selected == "report":
+    report()
+
+elif user_coffee_selected == "off":
+    new_coffee = False
+
+# TODO: 18 - Create a recursion function to ask user for another coffee:
+def recursion_coffee():
+
+    print(logo)
+
+    global user_coffee_selected
+
+    user_coffee_selected = ask_usr_selection()
+
+    if user_coffee_selected == "espresso" or user_coffee_selected == "latte" or user_coffee_selected == "cappuccino":
+
+        coffee_cost = MENU[user_coffee_selected]["cost"]
+
+        print(f"The {user_coffee_selected} costs $USD {coffee_cost}")
+
+        resources_status = check_resources(user_coffee_selected)
+
+        new_coffee = check_coins_function(resources_status)
+
+        return new_coffee
+
+    elif user_coffee_selected == "report":
+        report()
+
+    elif user_coffee_selected == "off":
+        return not new_coffee
+
+while new_coffee:
+    recursion_coffee()
+
+
+
+
+
+
